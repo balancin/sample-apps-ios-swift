16546b853d13e857a3b394458649d9223d1b1e9c
Fabio <balancin@gmail.com>
2017-01-04 07:32:45 -0200

    teste para ver alteracoes arquivos

--- BitriseSwiftSampleApp/ViewController.swift
+++ BitriseSwiftSampleApp/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let delta = 5.0
+        let frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        let mapView = MKMapView(frame:frame)
+        var region = MKCoordinateRegion()
+        region.center.latitude = 31.0
+        region.center.longitude = -110.0
+        region.span.latitudeDelta = delta
+        region.span.longitudeDelta = delta
+        mapView.setRegion(region, animated:true)
+        
+        self.view.addSubview(mapView)
+        
     }
 
     override func didReceiveMemoryWarning() {
